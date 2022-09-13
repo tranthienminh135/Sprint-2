252a1ba175aa405329942c86c49d8c1da90b66c3
tranthienminh135 <67372816+tranthienminh135@users.noreply.github.com>
2022-09-13 15:57:21 +0700

    update

--- my_project_spring/src/main/java/com/phuong/util/Test.java
+++
@@ -1,9 +0,0 @@
-package com.phuong.util;
-
-import java.text.DecimalFormat;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(new DecimalFormat("###,###,###.##").format(1.49244E7));
-    }
-}
